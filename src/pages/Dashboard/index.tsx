4447c3a4c8d69cef15543dbe6ba4982f85a561d7
Analista 1 <analista1@MERCADEOVIRTUAL.local>
2023-11-01 11:31:29 -0500

    Correct messages

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -35,7 +35,7 @@ const DashboardPage = () => {
               vertical: "top",
               horizontal: "right",
             },
-            variant: "error",
+            variant: "success",
           });
 
           formik.resetForm();
